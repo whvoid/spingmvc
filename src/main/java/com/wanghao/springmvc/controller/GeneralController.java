6fdc668d78afbf0e214773b08d39ad23e7d0c130
wanghao <87025569@qq.com>
2017-01-14 23:16:44 +0800

    hellenworld
    完成Hellenworld页面

---
+++ src/main/java/com/wanghao/springmvc/controller/GeneralController.java
@@ -0,0 +1,12 @@
+package com.wanghao.springmvc.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class GeneralController {
+@RequestMapping(value="index.do")  
+    public void index_jsp(Model model){  
+        model.addAttribute("hello", "Hi,SpringMVC");  
+        System.out.println("index.jsp");  
+    }
+}
